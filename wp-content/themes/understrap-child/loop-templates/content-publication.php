95427b4e1aa56c95be4f895878ba91f7a9c23d51
Phu Tran <tr.ngocphu@gmail.com>
2019-08-27 01:24:49 +0800

    sticky menu, update publication sort by year

--- wp-content/themes/understrap-child/loop-templates/content-publication.php
+++ wp-content/themes/understrap-child/loop-templates/content-publication.php
@@ -9,86 +9,71 @@
 defined( 'ABSPATH' ) || exit;
 
 $args = array (
-	'post_type' => array( 'publication' ),	
+	'post_type' => 'publication',	
 	'nopaging'  => true,
-	'order'     => 'DESC',
-	'orderby'   => 'year',
+	'meta_key'	=> 'year',
+	'orderby'	=> 'meta_value',
+	'order'		=> 'DESC'
 );
 
 $query_pub = new WP_Query( $args );
-
 $pubs = $query_pub->posts;
-
 wp_reset_postdata();
 
 ?>
 
 <header class="entry-header">
-
 	<h1 class="text-center">Publications</h1>
 	<div class="text-center mb-5"><< Only authors from our team are shown >></div>
-
 </header><!-- .entry-header -->
 
 <div class="pub-wrapper">
-
 	<?php foreach ( $pubs as $pub) : 
+		$author_ids = $pub->authors;		
+		$authors = [];	
 
-		$author_ids = $pub->authors;
-		
-		$authors = [];
-		
-		foreach ( $author_ids as $id ) {
-		
+		foreach ( $author_ids as $id ) {		
 			$author_query = new WP_User_Query( array( 'include' => array( (int)$id ) ) );
-		
 			array_push( $authors, $author_query->get_results()[0] );			
 		}
 		
-		$count = sizeof($authors);
-		
+		$count = sizeof($authors);		
 		$x = 0;
 
-		foreach ($authors as $author) {				
-			
+		foreach ($authors as $author) {
 			if ( $x < $count -1 ) {
-				$separator = " &#183; ";
-		
+				$separator = " &#183; ";		
 			} else {
-				$separator = "";
-		
+				$separator = "";		
 			}
 		
-			$authors[] = (object)array(
-		
+			$authors[] = (object)array(		
 				"name" => $author->display_name,
 				"link" => "/author/" . $author->user_login,
-				"separator" => $separator
-		
-			);
-		
+				"separator" => $separator		
+			);		
 			$x += 1;
-		};		
-		
+		};	
 		?>
 
 		<div class="row pub-item"> 
 			
 			<!-- Year : -->
-			<div class="col-xs-2 col-sm-3 col-md-2 col-lg-2 col-xl-1">
+			<div class="col-xs-3 col-sm-3 col-md-2 col-lg-2 col-xl-1">
 				<div class="pub-year"><?= $pub->year ?></div>
 			</div>
 			
 			<!-- Title, Author, Publisher : -->
-			<div class="col-xs-5 col-sm-9 col-md-10 col-lg-10 col-xl-11">
+			<div class="col-xs-9 col-sm-9 col-md-10 col-lg-10 col-xl-11">
 				<a href="<?= $pub->link ?>" target="_blank"><span class="pub-title"><?= $pub->article_title ?></span></a>
 				<div class="pub-author-list">
 					<?php foreach ( $authors as $author ) : ?>
-						<span class="each-author"><a href="<?= $author->link ?>"><?= $author->name ?></a><?= $author->separator ?></span>
+						<span class="pub-each-author"><a href="<?= $author->link ?>"><?= $author->name ?></a><?= $author->separator ?></span>
 					<?php endforeach; ?>
 				</div>
 				<p class="pub-publisher"><?= $pub->publisher ?></p>
 			</div>
+
 		</div>
 
 	<?php endforeach; ?>

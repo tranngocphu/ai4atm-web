275b416c47124c9a40c4634c40adf3bee41dc78d
tranngocphu <tr.ngocphu@gmail.com>
2020-04-28 06:51:09 -0500

    update yoast

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit42746c705929ac3daf11c1ac3d85c7c4::getLoader();
+return ComposerAutoloaderInit6cd015794c26ae72e6a76652322b517f::getLoader();
